67889c13950c230a220ac24934e5be3c6fdc9e66
Linus Sellberg <sellberg@gmail.com>
2022-05-15 10:38:18 +0200

    Cleanup

--- src/c/shim.c
+++ src/c/shim.c
@@ -8,7 +8,6 @@ extern inline unsigned _io_uring_cq_ready(struct io_uring *ring) {
   return io_uring_cq_ready(ring);
 }
 
-
 extern inline int _io_uring_wait_cqe_nr(struct io_uring *ring, struct io_uring_cqe **cqe_ptr, unsigned wait_nr) {
   return io_uring_wait_cqe_nr(ring, cqe_ptr, wait_nr);
 }
